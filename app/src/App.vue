a2090c6f0721355b8a9502f19c154b08149fe0af
xt3362 <81406950+xt3362@users.noreply.github.com>
2022-03-12 10:37:54 +0900

    fix filename 'Main' -> 'MainPage'

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Main from './components/Pages/Main.vue';
+import MainPage from './components/pages/MainPage.vue';
 </script>
 
 <template>
-  <Main />
+  <MainPage />
 </template>
 
 <style lang="scss">
--- app/vite.config.js
+++ app/vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    hmr:{
+      protocol:'ws',
+      host:'localhost'
+    }
+  },
   plugins: [vue()]
 })
